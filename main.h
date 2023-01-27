c59cb1df66888193248f8cb66112e5c943aefc85
MILWAUKEE MAEBELA <43237514+GitMaebela@users.noreply.github.com>
2023-01-27 23:47:54 +0200

    fix -test

--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #define _MAIN_H_
 
 int _printf(const char *format, ...);
-int number_of_digit(int number);
+int print_string(va_list valist);
+int print_char(char c);
+int print_number(int n);
 
 #endif
